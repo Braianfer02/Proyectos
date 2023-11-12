91cbbe7755ceb4450531276db43417fd9f1ae848
Braianfer02 <Braian.fer02@gmail.com>
2023-11-12 11:28:52 -0300

    probando

---
+++ ⭐/Calculator/src/main/java/braian/fernandez/calculator/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package braian.fernandez.calculator;
+
+/**
+ *
+ * @author Braia
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
